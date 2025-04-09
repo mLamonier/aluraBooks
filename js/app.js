915d42e667c9879319eae15d445890562fa125b0
unknown <miguel2002lamonier@gmail.com>
2025-04-09 12:16:46 -0300

    Att

--- js/app.js
+++ js/app.js
@@ -7,52 +7,81 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
-const menu = document.getElementById('imagem-menu');
-const listaMenu = document.getElementById('lista-menu');
+const menuHamburguer = document.getElementById('menu-hamburguer');
+const listaMenuCelular = document.getElementById('lista-menu-celular');
+const categoriasTablet = document.getElementById('categorias-tablet');
+const listaMenuTablet = document.getElementById('lista-menu-tablet');
 
-// Evento para exibir e ocultar o menu-hamburguer
-menu.addEventListener('click', function() {
-    let displayAtual = window.getComputedStyle(listaMenu).display;
+// Evento para exibir menu de categorias (Tela Tablet)
+categoriasTablet.addEventListener('click', function() {
+    let displayAtual = window.getComputedStyle(listaMenuTablet).display;
 
-    if (displayAtual === "none") {
-        listaMenu.style.display = "block";
-        menu.style.background = 'var(--azul-degrade)';
-        menu.src = 'img/Menu-Aberto.svg';
+    if (displayAtual === 'none'){
+        listaMenuTablet.style.display = "block";
     } else {
-        listaMenu.style.display = "none";
-        menu.style.background = 'var(--branco)';
-        menu.src = 'img/Menu.svg';
+        listaMenuTablet.style.display = "none";
     }
 });
 
-// Função para fechar o menu
+// Evento para exibir e ocultar o menu-hamburguer (Tela Celular)
+menuHamburguer.addEventListener('click', function() {
+    let displayAtual = window.getComputedStyle(listaMenuCelular).display;
+
+    if (displayAtual === "none") {
+        listaMenuCelular.style.display = "block";
+        menuHamburguer.style.background = 'var(--azul-degrade)';
+        menuHamburguer.src = 'img/Menu-Aberto.svg';
+    } else {
+        listaMenuCelular.style.display = "none";
+        menuHamburguer.style.background = 'var(--branco)';
+        menuHamburguer.src = 'img/Menu.svg';
+    }
+});
+
+function displayAtualCelular(){
+    let displayAtualCelular = window.getComputedStyle(listaMenuCelular).display;
+    return displayAtualCelular;
+}
+
+function displayAtualTablet(){
+    let displayAtualTablet = window.getComputedStyle(listaMenuTablet).display;
+    return displayAtualTablet;
+}
+
+// Função para fechar o menu (Tela Celular + Tablet)
 function fecharMenu() {
-    let displayAtual = window.getComputedStyle(listaMenu).display;
-    
-    if (displayAtual === "block") {
-        listaMenu.style.display = "none";
-        menu.style.background = 'var(--branco)';
-        menu.src = 'img/Menu.svg';
+    if (displayAtualCelular() === "block") {
+        listaMenuCelular.style.display = "none";
+        menuHamburguer.style.background = 'var(--branco)';
+        menuHamburguer.src = 'img/Menu.svg';
+        console.log("Acionado - Celular!")
+    }
+    if (displayAtualTablet() === "block") {
+        listaMenuTablet.style.display = "none";
+        console.log("Acionado - Tablet!")
     }
 };
 
-// Evento para identificar clique fora da lista
+// Evento para identificar clique fora da lista (Tela Celular + Tablet)
 document.addEventListener('click', function(event) {
-    let displayAtual = window.getComputedStyle(listaMenu).display;
-
-    if(displayAtual === "block"){
-        if (!listaMenu.contains(event.target) && !menu.contains(event.target)) {
+    if(displayAtualCelular() === "block"){
+        if (!listaMenuCelular.contains(event.target) && !menuHamburguer.contains(event.target)) {
             fecharMenu();
-            console.log("Acionado!")
+        }    
+    }
+    if(displayAtualTablet() === "block"){
+        if (!listaMenuTablet.contains(event.target) && !categoriasTablet.contains(event.target)) {
+            fecharMenu();
         }    
     }
 });
 
-// Evento de rolagem de página para fechar menu
+// Evento de rolagem de página para fechar menu (Tela Celular + Tablet)
 window.addEventListener('scroll', function() {
-    let displayAtual = window.getComputedStyle(listaMenu).display    
-    if(displayAtual === "block"){
+    if(displayAtualCelular() === "block"){
         fecharMenu();
-        console.log("Acionado!")
+    }
+    if(displayAtualTablet() === "block"){
+        fecharMenu();
     }
 });
